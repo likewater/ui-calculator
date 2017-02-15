43a54c852c4e143a12b9d4a6ac9e2d1bb789dd95
Blake Womack and Chris Wainner <chris@>
2017-02-15 09:28:45 -0800

    Add framework for input forms

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,29 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// var result = add(number1, number2);
+// alert(result);
+// var result = subtract(number1, number2);
+// alert(result);
+// var result = multiply(number1, number2);
+// alert(result);
+// var result = divide(number1, number2);
+// alert(result);
+
+$(document).ready(function(){
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  alert(add(number1, number2));
+});
